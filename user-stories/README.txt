e697f3f6986a98fc46ca99f140f786795b6f2b22
garethlangston <gareth.langston@gmail.com>
2016-02-29 14:40:23 +0000

    Adding User Stories Page

---
+++ user-stories/README.txt
@@ -0,0 +1,17 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/gareth-langston/pen/xVbGKO.
+
+ 
+
+Forked from [Nathan Edwards](http://codepen.io/cowfields/)'s Pen [pJWyqK](http://codepen.io/cowfields/pen/pJWyqK/).
+
+Forked from [Nathan Edwards](http://codepen.io/cowfields/)'s Pen [pJWyqK](http://codepen.io/cowfields/pen/pJWyqK/).
+
+Forked from [Nathan Edwards](http://codepen.io/cowfields/)'s Pen [pJWyqK](http://codepen.io/cowfields/pen/pJWyqK/).
+
+Forked from [Nathan Edwards](http://codepen.io/cowfields/)'s Pen [pJWyqK](http://codepen.io/cowfields/pen/pJWyqK/).
+
+Forked from [Nathan Edwards](http://codepen.io/cowfields/)'s Pen [pJWyqK](http://codepen.io/cowfields/pen/pJWyqK/).
+
+Forked from [Nathan Edwards](http://codepen.io/cowfields/)'s Pen [pJWyqK](http://codepen.io/cowfields/pen/pJWyqK/).
+
+Forked from [Nathan Edwards](http://codepen.io/cowfields/)'s Pen [pJWyqK](http://codepen.io/cowfields/pen/pJWyqK/).
---
+++ user-stories/js/index.js
@@ -0,0 +1,26 @@
+var gid = '1rKHQaFxTQigOT3CBV5Xf68qNbFvgZh7unok4oH-8Od0';
+var url = 'https://spreadsheets.google.com/feeds/list/' + gid + '/1/public/values?alt=json';
+
+// AJAX call to the GDOCS JSON
+$.ajax({
+  url: url,
+  dataType: 'JSON'
+}).done(function(response) {
+  
+  // response is the full response from google.  It's a huge object of data including author and stuff, so you need to sift through and find what you need...
+  console.log(response);
+  
+  // response.feed.entry is where the actual rows are, and this is an array of each row, grouped by the first row.  I don't know how google knows to do it like this, it just does.
+  var data = response.feed.entry;
+  var number = 1;
+  
+  // loop through the data array
+  data.forEach(function( row ) {
+    // the data comes in the format where $t is the actual piece of data which is a property of the first row's name...
+    number = number + 1;
+    
+    $('#results').append( '<li class="story ' + row.gsx$asa.$t + '"><p class="tiny">' + row.gsx$timestamp.$t + '</p><p class="big asa"><strong>As a: </strong>' + row.gsx$asa.$t + '</p><p class="big ineed"><strong>I need: </strong>' + row.gsx$userneed.$t + '</p><p class="big sothat"><strong>So that: </strong>' + row.gsx$sothat.$t + '</p></li>');
+    
+  });
+    
+});
